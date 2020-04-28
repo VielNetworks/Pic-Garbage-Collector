8a38cb1cb4005fca73ae151bd7831b0ebf26abfb
vielnet <64392089+vielnet@users.noreply.github.com>
2020-04-28 19:49:45 -0400

    Update StewUtils.h

--- src/StewUtils.h
+++ src/StewUtils.h
@@ -1,1 +1,16 @@
+#pragma once
+#ifndef STEWUTILS_H
+#define STEWUTILS_H
 
+#include "StewNode.h"
+
+namespace StewGC
+{
+    class Utils
+    {
+        public:
+            static void ThreadSpin();
+    };
+}
+
+#endif // STEWUTILS_H
