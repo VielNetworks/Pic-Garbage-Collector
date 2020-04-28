51df37682a468c205129e1701f181c29c6e24cd5
vielnet <64392089+vielnet@users.noreply.github.com>
2020-04-28 19:51:44 -0400

    Update StewUtils.cpp

--- src/StewUtils.cpp
+++ src/StewUtils.cpp
@@ -1,1 +1,16 @@
+#include "StewUtils.h"
+#include <chrono>
+#include <thread>
 
+use namespace StewGC;
+
+//********************************************************************************
+
+void Utils::ThreadSpin()
+{
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = start + std::chrono::microseconds(100);
+    do std::this_thread::yield(); while (std::chrono::high_resolution_clock::now() < end);
+}
+
+//********************************************************************************
